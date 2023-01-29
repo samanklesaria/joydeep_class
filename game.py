bc04f5b6202e2a6b68a81c9c671dae0e4551a379
Sam Anklesaria <anklesaria@utexs.edu>
2023-01-29 10:16:51 -0600

    Initial commit

---
+++ game.py
@@ -0,0 +1,351 @@
+import numpy as np
+from collections import namedtuple
+import matplotlib.pyplot as plt
+
+def printstate(locs):
+    itr = iter(locs)
+    print("White Blocks")
+    for a in range(5):
+        print(next(itr))
+    print("While ball", next(itr))
+    print("Black Blocks")
+    for a in range(5):
+        print(next(itr))
+    print("Black ball", next(itr))
+
+
+x_ticks = np.arange(0, 7)
+y_ticks = np.arange(0, 8)
+
+def plotstate(locs):
+    itr = iter(locs)
+    for a in range(5):
+        x,y = next(itr)
+        plt.scatter([x], [y], c='r', s=100)
+    x,y = next(itr)
+    plt.scatter([x], [y], c='r', marker='s', s=100)
+    for a in range(5):
+        x,y = next(itr)
+        plt.scatter([x], [y], c='b', s=100)
+    x,y = next(itr)
+    plt.scatter([x], [y], c='b', marker='s', s=100)
+    ax = plt.gca()
+    ax.set_xticks(x_ticks)
+    ax.set_yticks(y_ticks)
+    plt.axis('equal')
+    plt.grid(which='both')
+    plt.show()
+
+def block_actions(st, ix):
+    return [st.decode_single_pos(y) for y in Rules.single_piece_actions(st, ix)]
+
+class BoardState:
+    """
+    Represents a state in the game
+    """
+
+    def __init__(self, state=None):
+        """
+        Initializes a fresh game state
+        """
+        self.N_ROWS = 8
+        self.N_COLS = 7
+
+        if state is not None:
+            self.state = np.array(state)
+        else:
+            self.state = np.array([1,2,3,4,5,3,50,51,52,53,54,52])
+
+        
+        self.locs1 = np.arange(5) # player 1 locs
+        self.locs2 = 6 + self.locs1 # player 2 locs
+        self.b = [5,11] # player ball ixs
+        self.block_locs = np.concatenate([self.locs1, self.locs2])
+
+        self.stated = np.stack([np.array(self.decode_single_pos(d)) for d in self.state])
+
+    # Numpy is easier to work with than arrays of tuples.
+    # But the tests target the stupid interface. This translates.
+    @property
+    def decode_state(self):
+        return [self.decode_single_pos(d) for d in self.state]
+
+    @decode_state.setter
+    def decode_state(self, value):
+        self.stated = np.stack([np.array(a) for a in value])
+
+    def update(self, idx, val):
+        """
+        Updates both the encoded and decoded states
+        """
+        self.state[idx] = val
+        self.stated[idx, :] = np.array(self.decode_single_pos(self.state[idx]))
+        self.decode_state[idx] = self.decode_single_pos(self.state[idx])
+
+    def make_state(self):
+        """
+        Creates a new decoded state list from the existing state array
+        """
+        return [self.decode_single_pos(d) for d in self.state]
+
+    def encode_single_pos(self, cr: tuple):
+        """
+        Encodes a single coordinate (col, row) -> Z
+
+        Input: a tuple (col, row)
+        Output: an integer in the interval [0, 55] inclusive
+        """
+        return cr[0] + cr[1] * self.N_COLS 
+
+    def decode_single_pos(self, n: int):
+        """
+        Decodes a single integer into a coordinate on the board: Z -> (col, row)
+
+        Input: an integer in the interval [0, 55] inclusive
+        Output: a tuple (col, row)
+        """
+        return (n % self.N_COLS, n // self.N_COLS)
+
+    def is_termination_state(self):
+        """
+        Checks if the current state is a termination state. Termination occurs when
+        one of the player's move their ball to the opposite side of the board.
+        """
+        if not self.is_valid(): return False
+        r0 = self.stated[self.b[0]][1]
+        r1 = self.stated[self.b[1]][1]
+        return r0 == (self.N_ROWS - 1) or r1 == 0 
+
+    def is_valid(self):
+        """
+        Checks if a board configuration is valid. This function checks whether the current
+        value self.state represents a valid board configuration or not. This encodes and checks
+        the various constrainsts that must always be satisfied in any valid board state during a game.
+
+        If we give you a self.state array of 12 arbitrary integers, this function should indicate whether
+        it represents a valid board configuration.
+
+        Output: return True (if valid) or False (if not valid)
+        """
+
+        outcome = True
+
+        # All 12 pieces must be on the board (in [0,55])
+        outcome &= (self.state >= 0).all() and (self.state <= 55).all()
+        outcome &= len(self.state) == 12
+
+        # Two blocks cannot occupy the same space
+        block_state = self.state[self.block_locs]
+        outcome &= len(np.unique(block_state)) == len(block_state)
+
+        # Each ball must be held by a block of the same color
+        outcome &= (self.state[self.locs1] == self.state[self.b[0]]).any()
+        outcome &= not (self.state[self.locs2] == self.state[self.b[0]]).any()
+        outcome &= (self.state[self.locs2] == self.state[self.b[1]]).any()
+        outcome &= not (self.state[self.locs1] == self.state[self.b[1]]).any()
+        return outcome
+
+    def occupied(self, y):
+        return (self.state == y).any()
+
+PotentialMove = namedtuple('PotentialMove', ['norm', 'player', 'y'])
+
+def lub(a: PotentialMove, b: PotentialMove, hometeam):
+    if a.norm < b.norm:
+        return a
+    if a.norm == b.norm:
+        assert False
+        if a.player == hometeam:
+            return b
+        if b.player == hometeam:
+            return a
+    return b
+
+class Rules:
+
+    @staticmethod
+    def single_piece_actions(st, piece_ix):
+        """
+        Returns the set of possible actions for the given piece, assumed to be a valid piece located
+        at piece_idx in the board_state.state.
+
+        Inputs:
+            - board_state, assumed to be a BoardState
+            - piece_idx, assumed to be an index into board_state, identfying which piece we wish to
+              enumerate the actions for.
+
+        Output: an iterable (set or list or tuple) of integers which indicate the encoded positions
+            that piece_idx can move to during this turn.
+        """
+
+        if st.state[st.b[piece_ix // 6]] != st.state[piece_ix]:
+            x = st.stated[piece_ix]
+            for move in (np.array([1, 2]), np.array([2, 1])):
+                for d1 in (-1, 1):
+                    for d2 in (-1, 1):
+                        pos = x + move * np.array([d1, d2])
+                        if (pos >= 0).all() and (pos <= np.array([6, 7])).all():
+                            y = st.encode_single_pos(pos)
+                            if not st.occupied(y):
+                                yield y
+
+    @staticmethod
+    def single_ball_actions(st, player_ix):
+        """
+        Returns the set of possible actions for moving the specified ball, assumed to be the
+        valid ball for player_idx  in the board_state
+
+        Inputs:
+            - board_state, assumed to be a BoardState
+            - player_idx, either 0 or 1, to indicate which player's ball we are enumerating over
+        
+        Output: an iterable (set or list or tuple) of integers which indicate the encoded positions
+            that player_idx's ball can move to during this turn.
+        """
+        return Rules.ball_actions_from(st, st.state[st.b[player_ix]], player_ix)
+
+    @staticmethod
+    def ball_actions_from(st, y, player_ix):
+        # Look where we can pass ball. Then from there, look for any new places we can pass. And so on. 
+        passes = set([y])
+
+        def recurse(x: np.ndarray):
+            for y in Rules.pass_actions(st, player_ix, x, passes):
+                passes.add(y)
+                recurse(np.array(st.decode_single_pos(y)))
+
+        recurse(np.array(st.decode_single_pos(y)))
+        passes.remove(y)
+        return passes
+        
+    @staticmethod
+    def pass_actions(st, player_ix, x, passes): 
+        # For each person, get their vector to the ball.
+        # Map each (vector, team) to its unit length version. Reverse the mapping, using lub defined above.
+        # Then yield all the remaining vectors for the current team.
+
+        ys = st.state[st.block_locs]
+        vecs = st.stated[st.block_locs, :] - x[None,:]
+        absvecs = np.abs(vecs)
+        diagonals = absvecs[:, 0] == absvecs[:, 1]
+        verticals = vecs[:, 0] == 0
+        horizontals = vecs[:, 1] == 0
+        norms = absvecs.max(axis=1)
+        nz = norms > 0
+        valid = (diagonals | verticals | horizontals) & nz
+
+        players = np.floor_divide(st.block_locs[valid], 6)
+        units = (tuple(u) for u in
+            np.floor_divide(vecs[valid], norms[valid,None]))
+        moves = dict()
+        for (y, n, p, u) in zip(ys[valid], norms[valid], players, units):
+            newmove = PotentialMove(n, p, y)
+            if u in moves:
+                moves[u] = lub(moves[u], newmove, player_ix)
+            else:
+                moves[u] = newmove
+
+        def yielder():
+            for (k,v) in moves.items():
+                if v.player == player_ix and v.y not in passes:
+                    yield v.y
+        return list(yielder())
+
+class GameSimulator:
+    """
+    Responsible for handling the game simulation
+    """
+
+    def __init__(self, players):
+        self.game_state = BoardState()
+        self.current_round = -1 ## The game starts on round 0; white's move on EVEN rounds; black's move on ODD rounds
+        self.players = players
+
+    def run(self):
+        """
+        Runs a game simulation
+        """
+        while not self.game_state.is_termination_state():
+            ## Determine the round number, and the player who needs to move
+            self.current_round += 1
+            player_idx = self.current_round % 2
+            ## For the player who needs to move, provide them with the current game state
+            ## and then ask them to choose an action according to their policy
+            action, value = self.players[player_idx].policy( self.game_state.make_state() )
+            print(f"Round: {self.current_round} Player: {player_idx} State: {tuple(self.game_state.state)} Action: {action} Value: {value}")
+
+            if not self.validate_action(action, player_idx):
+                ## If an invalid action is provided, then the other player will be declared the winner
+                if player_idx == 0:
+                    return self.current_round, "BLACK", "White provided an invalid action"
+                else:
+                    return self.current_round, "WHITE", "Black probided an invalid action"
+
+            ## Updates the game state
+            self.update(action, player_idx)
+
+        ## Player who moved last is the winner
+        if player_idx == 0:
+            return self.current_round, "WHITE", "No issues"
+        else:
+            return self.current_round, "BLACK", "No issues"
+
+    def generate_valid_actions(self, player_ix: int):
+        """
+        Given a valid state, and a player's turn, generate the set of possible actions that player can take
+
+        player_idx is either 0 or 1
+
+        Input:
+            - player_idx, which indicates the player that is moving this turn. This will help index into the
+              current BoardState which is self.game_state
+        Outputs:
+            - a set of tuples (relative_idx, encoded position), each of which encodes an action. The set should include
+              all possible actions that the player can take during this turn. relative_idx must be an
+              integer on the interval [0, 5] inclusive. Given relative_idx and player_idx, the index for any
+              piece in the boardstate can be obtained, so relative_idx is the index relative to current player's
+              pieces. Pieces with relative index 0,1,2,3,4 are block pieces that like knights in chess, and
+              relative index 5 is the player's ball piece.
+            
+        """
+        def iterator():
+            for i in range(5):
+                piece = 6 * player_ix + i
+                for action in Rules.single_piece_actions(self.game_state, piece):
+                   yield (i, action)
+            for action in Rules.single_ball_actions(self.game_state, player_ix):
+                yield (5, action)
+        return list(iterator())
+        
+    def validate_action(self, action: tuple, player_idx: int):
+        """
+        Checks whether or not the specified action can be taken from this state by the specified player
+
+        Inputs:
+            - action is a tuple (relative_idx, encoded position)
+            - player_idx is an integer 0 or 1 representing the player that is moving this turn
+            - self.game_state represents the current BoardState
+
+        Output:
+            - if the action is valid, return True
+            - if the action is not valid, raise ValueError
+        """
+        assert player_idx in [0,1]
+        assert action[0] < 6
+        piece_ix = player_idx * 6 + action[0]
+        if action[0] == 5:
+            if action[1] not in Rules.ball_actions_from(self.game_state, self.game_state.state[piece_ix], player_idx):
+                raise ValueError("Cannot move a ball that way")
+        else:
+            if action[1] not in set(Rules.single_piece_actions(self.game_state, piece_ix)):
+                raise ValueError("Cannot move a block that way")
+        return True
+    
+    def update(self, action: tuple, player_idx: int):
+        """
+        Uses a validated action and updates the game board state
+        """
+        offset_idx = player_idx * 6 ## Either 0 or 6
+        idx, pos = action
+        self.game_state.update(offset_idx + idx, pos)
+
---
+++ search.py
@@ -0,0 +1,158 @@
+import numpy as np
+from queue import Queue
+from game import BoardState, GameSimulator, Rules, plotstate
+
+VALIDATE = False
+
+def view_solution(steps):
+    for (state, action) in steps:
+        st = BoardState(state[0])
+        print("Player", state[1])
+        if action is not None:
+            print("Action", st.decode_single_pos(action[1]))
+        plotstate(st.stated)
+
+
+class Problem:
+
+    def __init__(self, initial_state, goal_state_set: set):
+        self.initial_state = initial_state
+        self.goal_state_set = goal_state_set
+
+    def get_actions(self, state):
+        """
+        Returns a set of valid actions that can be taken from this state
+        """
+        pass
+
+    def execute(self, state, action):
+        """
+        Transitions from the state to the next state that results from taking the action
+        """
+        pass
+
+    def is_goal(self, state):
+        """
+        Checks if the state is a goal state in the set of goal states
+        """
+        return state in self.goal_state_set
+
+
+class GameStateProblem(Problem):
+
+    def __init__(self, initial_board_state, goal_board_state, player_idx):
+        """
+        player_idx is 0 or 1, depending on which player will be first to move from this initial state.
+
+        The form of initial state is:
+        ((game board state tuple), player_idx ) <--- indicates state of board and who's turn it is to move
+        """
+        super().__init__(tuple((tuple(initial_board_state.state), player_idx)), set([tuple((tuple(goal_board_state.state), 0)), tuple((tuple(goal_board_state.state), 1))]))
+        self.sim = GameSimulator(None)
+        self.search_alg_fnc = None
+        self.set_search_alg()
+
+    def validate_path(self, path):
+        new_st = None
+        for (st, action) in path:
+            assert new_st == st or new_st is None
+            if action is not None:
+                new_st = self.execute(st, action)
+
+    def set_search_alg(self, alg="bfs"):
+        """
+        If you decide to implement several search algorithms, and you wish to switch between them,
+        pass a string as a parameter to alg, and then set:
+            self.search_alg_fnc = self.your_method
+        to indicate which algorithm you'd like to run.
+        """
+        self.search_alg_fnc = self.bfs
+
+    def get_actions(self, state: tuple):
+        """
+        From the given state, provide the set possible actions that can be taken from the state
+
+        Inputs: 
+            state: (encoded_state, player_idx), where encoded_state is a tuple of 12 integers,
+                and player_idx is the player that is moving this turn
+
+        Outputs:
+            returns a set of actions
+        """
+        s, p = state
+        np_state = np.array(s)
+        self.sim.game_state.state = np_state
+        self.sim.game_state.decode_state = self.sim.game_state.make_state()
+
+        return self.sim.generate_valid_actions(p)
+
+    def execute(self, state: tuple, action: tuple):
+        """
+        From the given state, executes the given action
+
+        The action is given with respect to the current player
+
+        Inputs: 
+            state: is a tuple (encoded_state, player_idx), where encoded_state is a tuple of 12 integers,
+                and player_idx is the player that is moving this turn
+            action: (relative_idx, position), where relative_idx is an index into the encoded_state
+                with respect to the player_idx, and position is the encoded position where the indexed piece should move to.
+        Outputs:
+            the next state tuple that results from taking action in state
+        """
+        s, p = state
+        k, v = action
+        offset_idx = p * 6
+        return tuple((tuple( s[i] if i != offset_idx + k else v for i in range(len(s))), (p + 1) % 2))
+
+
+    def bfs(self):
+        frontier = Queue()
+        frontier.put(self.initial_state)
+        parent = {}
+        parent[self.initial_state] = (None, None)
+        while not frontier.empty():
+            current = frontier.get()
+            if current in self.goal_state_set:
+                backwards = list(get_path(current, parent, None))
+                backwards.reverse()
+                if VALIDATE:
+                    self.validate_path(backwards)
+                return backwards
+            for action in self.get_actions(current):
+                if VALIDATE:
+                    self.sim.validate_action(action, current[1])
+                succ = self.execute(current, action)
+                if not (succ in parent):
+                    frontier.put(succ)
+                    parent[succ] = (current, action)
+
+def get_path(current, parent, action):
+    if current is not None:
+        yield (current, action)
+        pred, action = parent[current]
+        yield from get_path(pred, parent, action)
+
+    ## TODO: Implement your search algorithm(s) here as methods of the GameStateProblem.
+    ##       You are free to specify parameters that your method may require.
+    ##       However, you must ensure that your method returns a list of (state, action) pairs, where
+    ##       the first state and action in the list correspond to the initial state and action taken from
+    ##       the initial state, and the last (s,a) pair has s as a goal state, and a=None, and the intermediate
+    ##       (s,a) pairs correspond to the sequence of states and actions taken from the initial to goal state.
+    ## NOTE: The format of state is a tuple: (encoded_state, player_idx), where encoded_state is a tuple of 12 integers
+    ##       (mirroring the contents of BoardState.state), and player_idx is 0 or 1, indicating the player that is
+    ##       moving in this state.
+    ##       The format of action is a tuple: (relative_idx, position), where relative_idx the relative index into encoded_state
+    ##       with respect to player_idx, and position is the encoded position where the piece should move to with this action.
+    ## NOTE: self.get_actions will obtain the current actions available in current game state.
+    ## NOTE: self.execute acts like the transition function.
+    ## NOTE: Remember to set self.search_alg_fnc in set_search_alg above.
+    ## 
+    """ Here is an example:
+    
+    def my_snazzy_search_algorithm(self):
+        ## Some kind of search algorithm
+        ## ...
+        return solution ## Solution is an ordered list of (s,a)
+    """
+
