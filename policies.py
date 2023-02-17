14bd938a16889697e0c6aa6ac6e59e953fe41726
Sam Anklesaria <anklesaria@utexs.edu>
2023-02-17 11:40:47 -0600

    Add MCTS

--- policies.py
+++ policies.py
@@ -20,9 +20,11 @@ class Policy:
     def actions(self, state: BoardState, player: PlayerIx) -> List[Action]:
         return list(game.generate_valid_actions(state, player))
 
-class PassivePolicy(Policy):
+class Passive:
     def actions(self, state: BoardState, player: PlayerIx) -> List[Action]:
-        return list(game.generate_valid_actions(state, player, False))
+        actions = list(game.generate_valid_actions(state, player, False))
+        random.shuffle(actions)
+        return actions
 
 class HueristicPolicy(Policy):
     def actions(self, state, player):
@@ -100,11 +102,80 @@ class AlphaBeta(HueristicPolicy):
         else:
             return self.min_action(state, 1, no_max_action, no_min_action)
 
+class PassiveAlphaBeta(Passive, AlphaBeta):
+    pass
+
+class PassiveMinimax(Passive, MinimaxPolicy):
+    pass
+
 class RandPolicy(Policy):
     def policy(self, state):
         actions = self.actions(state, self.player)
         chosen = random.randrange(len(actions))
         return (actions[chosen], 0)
 
+class Edge(NamedTuple):
+    action: Optional[Action]
+    q: float
+    n: int
+    parent_n: int
+
+def get_ucb(e: Edge):
+    return e.q + math.sqrt(2) * math.sqrt(math.log(e.parent_n), e.n)
+
+class MCTS(HueristicPolicy):
+    def __init__(self, player, limit=500):
+        self.player = player
+        self.cache = dict()
+        self.limit = limit
+
+    # The minimizing player is 1
+    def min_action(self, state: BoardState):
+        statekey = tuple(state.state) # TODO: remove symmetries
+        if state.is_termination_state():
+            self.cache[statekey] = Edge(None, 1, 1, 1)
+        if statekey in self.cache:
+            choice = min(self.cache[statekey], key=get_ucb)
+            return self.max_action(game.next_state(state, choice.action, 1))
+        else:
+            self.rollout(state)
+
+    # The maximizing player is 0
+    def max_action(self, state: BoardState):
+        statekey = tuple(state.state) # TODO: remove symmetries
+        if state.is_termination_state():
+            self.cache[statekey] = Edge(None, -1, 1, 1)
+        elif statekey in self.cache:
+            choice = max(self.cache[statekey], key=get_ucb)
+            self.min_action(game.next_state(state, choice.action, 0))
+        else:
+            self.rollout(state)
+
+    def rollout(self, state):
+        pass # TODO
+
+    def lookup_action(self, state, a):
+        cached_val = self.cache[game.next_state(state, a)]
+        return ValuedAction(cached_val.action, cached_val.q)
+
+    def policy(self, state):
+        for _ in range(self.limit):
+            if self.player == 0:
+                self.max_action(state, 1)
+            else:
+                self.min_action(state, 1)
+        actions = [self.lookup_action(state, a)
+            for a in self.actions(state, self.player)]
+        if self.player == 0:
+            return max(actions, key=get_value)
+        else:
+            return min(actions, key=get_value)
+
+
+
 # For MCTS, we should map to a space without symmetries.
-# Perhaps flip the board to the orientation with the highest hash. 
+# Perhaps flip the board to the orientation with the highest hash.
+
+# Perhaps we could run MCTS for a long time without clearing its
+# cache much. Then train a value function on its output to use as a hueristic
+# for AlphaBeta
